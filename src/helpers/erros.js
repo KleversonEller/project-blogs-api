384cbbc92f7978e87a0fccd82b573b95ef6abd2e
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-19 01:33:42 -0300

    feat: :sparkles: Criando o middleware de erro
    Criando o middleware de erro e um helper com erros personalizados

---
+++ src/helpers/erros.js
@@ -0,0 +1,4 @@
+module.exports = {
+    required: { error: { code: 'required', message: 'Some required fields are missing' } },
+    invalidData: { error: { code: 'invalidData', message: 'Invalid fields' } },
+};
---
+++ src/middleware/error.js
@@ -0,0 +1,23 @@
+const statusByErrorCode = {
+    'any.required': 400,
+    'string.min': 422,
+    'number.positive': 422,
+    'number.min': 422,
+    'string.empty': 400,
+    notFound: 404,
+    required: 400,
+    invalidData: 400,
+    alreadyExists: 409,
+};
+module.exports = (err, _req, res, _next) => {
+    if (err.isJoi) {
+        const status = statusByErrorCode[err.details[0].type] || 500;
+        return res.status(status).json({ message: err.details[0].message });
+    }
+    if (err.code) {
+        const status = statusByErrorCode[err.code] || 500;
+        return res.status(status).json(err);
+    }
+    console.error(err);
+    return res.status(500).json({ message: 'Internal server error' });
+};
