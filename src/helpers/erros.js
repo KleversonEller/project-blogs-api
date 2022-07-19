91e6a3261d6283d62120eac6c81cfed4ca6e915c
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-19 06:01:05 -0300

    refactor: :recycle: Adicionando novo erro
    Adicionando novo erro personalizado na lista de erros

--- src/helpers/erros.js
+++ src/helpers/erros.js
@@ -2,4 +2,5 @@ module.exports = {
     required: { error: { code: 'required', message: 'Some required fields are missing' } },
     invalidData: { error: { code: 'invalidData', message: 'Invalid fields' } },
     duplicity: { error: { code: 'alreadyExists', message: 'User already registered' } },
+    userNotFound: { error: { code: 'notFound', message: 'User does not exist' } },
 };
