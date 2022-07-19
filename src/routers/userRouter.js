3ac35daa9564d8ac8fbeb9f54599e39dfa443536
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-19 05:41:37 -0300

    feat: :sparkles: Criando GET user router
    Criando a rota que traz todos os usuarios cadastrados no DB validando se a requisição esta sendo feita por um usuario autorizado

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const express = require('express');
 const controller = require('../controllers/index');
+const validToken = require('../middleware/auth/validToken');
 
 router.use(express.json());
 
+router.get('/', validToken, controller.user.getAll);
 router.post('/', controller.user.create);
 
 module.exports = router;
