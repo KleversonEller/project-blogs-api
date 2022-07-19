2d75a5535f799d9a5753075359c57e7e8477da99
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-19 19:01:47 -0300

    refactor: :recycle: Refatorando blogPost model
    Refatorando blogPost model para atender os requsitos dos testes

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const BlogPosts = sequelize.define('BlogPosts', {
+    const BlogPost = sequelize.define('BlogPost', {
         id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
         title: DataTypes.STRING,
         content: DataTypes.STRING,
@@ -11,9 +11,9 @@ module.exports = (sequelize, DataTypes) => {
             timestamps: false,
         });
 
-    BlogPosts.associate = (models) => {
-        BlogPosts.belongsTo(models.User, { foreignKey: 'userId', as: 'Users' });
+    BlogPost.associate = (models) => {
+        BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'Users' });
     };
 
-    return BlogPosts;
+    return BlogPost;
 };
