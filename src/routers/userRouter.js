9a9c770217bbfd42bba8ba998e3d0ae17d847a6d
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-19 06:08:27 -0300

    feat: :sparkles: Criando GET user/:id router
    Criando a rota para buscar por um usuario a partir do seu ID

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,6 +6,7 @@ const validToken = require('../middleware/auth/validToken');
 router.use(express.json());
 
 router.get('/', validToken, controller.user.getAll);
+router.get('/:id', validToken, controller.user.getForId);
 router.post('/', controller.user.create);
 
 module.exports = router;
