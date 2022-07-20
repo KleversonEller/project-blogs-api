603e2d9c9293a8bdca83ad86cbfa4de0d5e75990
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-20 17:44:28 -0300

    feat: :sparkles: Criando post (create) router
    Criando a rota '/post' (create) resposavel pos efetuar a criação de um novo post no DB

--- src/api.js
+++ src/api.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use('/login', router.login);
 app.use('/user', router.user);
 app.use('/categories', router.category);
+app.use('/post', router.post);
 
 app.use(errorMiddleware);
 
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,6 @@
 const login = require('./loginRouter');
 const user = require('./userRouter');
 const category = require('./categoryRouter');
+const post = require('./postsRouter');
 
-module.exports = { login, user, category };
+module.exports = { login, user, category, post };
